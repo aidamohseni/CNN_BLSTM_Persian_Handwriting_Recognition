47c8d060280199b0e7d98d77c2cc13a6e55b9b36
Aida Mohseni <aidamohsenimail@gmail.com>
2023-10-09 16:48:40 +0330

    Add files via upload

---
+++ Persian Handwriting Recognition_BLSTM_CNN.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 23:46:26 2018
+
+@author: Aida Mohseni aidamohseni@gmail.com
+
+"""
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense, TimeDistributed
+from keras import backend as K
+from keras.layers import Bidirectional, LSTM
+
+
+# dimensions of our images.
+img_width, img_height = 28, 28
+pool_size = 4
+train_data_dir = 'd:/data/train'
+validation_data_dir = 'd:/data/validation'
+nb_train_samples = 1600
+nb_validation_samples = 400
+epochs = 50
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+############################################
+model.add(TimeDistributed(Flatten()))
+model.add(Bidirectional(LSTM(64)))
+model.add(Dropout(0.25))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+###########################################
+
+'''model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))'''
+###########################################
+
+
+model.compile(loss='binary_crossentropy',
+             # optimizer='rmsprop',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+model.save_weights('first_try.h5')
+
